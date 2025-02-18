c1f631b4a225f8be39e2844c76a091895b8d9621
Kmirka <147250112+Kmirka@users.noreply.github.com>
2025-02-18 19:20:31 +0200

    Create some.py

---
+++ postmanAPP/some.py
@@ -0,0 +1,27 @@
+from mailjet_rest import Client
+import os
+
+api_key = os.getenv('API_KEY')
+api_secret = os.getenv('API_SECRET')
+mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+def sendMesage(Subject, text='', additional=''):
+	message = {
+	'Messages': [
+					{
+						"From": {
+								"Email": "arturroman4uk@gmail.com",
+								"Name": "Me"
+						},
+						"To": [
+								{
+								"Email": "arturroman4uk@gmail.com",
+								"Name": "You"
+								}
+						],
+						"Subject": Subject,
+						"TextPart": text,
+						"HTMLPart": additional
+					}
+			]
+	}
+	return message
